01d9c403f25ada99e039bd78407ff874b6dd93f0
Jarrod Norwell <official.antique@gmail.com>
2024-05-31 10:02:07 +0200

    Added HDCP authentication state event and stubs

--- src/core/hle/service/am/service/common_state_getter.cpp
+++ src/core/hle/service/am/service/common_state_getter.cpp
@@ -48,8 +48,8 @@ ICommonStateGetter::ICommonStateGetter(Core::System& system_, std::shared_ptr<Ap
         {59, nullptr, "SetVrPositionForDebug"},
         {60, D<&ICommonStateGetter::GetDefaultDisplayResolution>, "GetDefaultDisplayResolution"},
         {61, D<&ICommonStateGetter::GetDefaultDisplayResolutionChangeEvent>, "GetDefaultDisplayResolutionChangeEvent"},
-        {62, nullptr, "GetHdcpAuthenticationState"},
-        {63, nullptr, "GetHdcpAuthenticationStateChangeEvent"},
+        {62, D<&ICommonStateGetter::GetHdcpAuthenticationState>, "GetHdcpAuthenticationState"},
+        {63, D<&ICommonStateGetter::GetHdcpAuthenticationStateChangeEvent>, "GetHdcpAuthenticationStateChangeEvent"},
         {64, nullptr, "SetTvPowerStateMatchingMode"},
         {65, nullptr, "GetApplicationIdByContentActionName"},
         {66, &ICommonStateGetter::SetCpuBoostMode, "SetCpuBoostMode"},
@@ -137,6 +137,19 @@ Result ICommonStateGetter::GetDefaultDisplayResolutionChangeEvent(
     OutCopyHandle<Kernel::KReadableEvent> out_event) {
     LOG_DEBUG(Service_AM, "called");
     *out_event = m_applet->lifecycle_manager.GetOperationModeChangedSystemEvent().GetHandle();
+    R_SUCCEED();
+}
+
+Result ICommonStateGetter::GetHdcpAuthenticationState(Out<s32> out_state) {
+    LOG_DEBUG(Service_AM, "called");
+    *out_state = 1;
+    R_SUCCEED();
+}
+
+Result ICommonStateGetter::GetHdcpAuthenticationStateChangeEvent(
+    OutCopyHandle<Kernel::KReadableEvent> out_event) {
+    LOG_DEBUG(Service_AM, "called");
+    *out_event = m_applet->lifecycle_manager.GetHDCPStateChangedEvent().GetHandle();
     R_SUCCEED();
 }
 
